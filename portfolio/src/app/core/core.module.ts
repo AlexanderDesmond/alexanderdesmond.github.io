1ebb0358ab29303e89d258d85172b5d92f63ba75
Alex <AlexanderDesmond848898@gmail.com>
2020-01-10 17:35:28 +1100

    Add background image to homepage.

--- portfolio/src/app/core/core.module.ts
+++ portfolio/src/app/core/core.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from "@angular/common";
 
 import { AngularMaterialModule } from "../shared/angular-material.module";
 import { HeaderComponent } from "./components/header/header.component";
+import { HomeComponent } from "./components/home/home.component";
+import { ProjectsComponent } from './components/projects/projects.component';
+import { MeComponent } from './components/me/me.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, HomeComponent, ProjectsComponent, MeComponent],
   imports: [CommonModule, AngularMaterialModule],
-  exports: [HeaderComponent]
+  exports: [HeaderComponent, HomeComponent]
 })
 export class CoreModule {}
